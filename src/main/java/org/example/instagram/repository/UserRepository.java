b29e2dcb34de9da5c76c74acf89ed4db0d60dcf9
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-12-01 15:37:36 +0900

    login 기능 구현

--- src/main/java/org/example/instagram/repository/UserRepository.java
+++ src/main/java/org/example/instagram/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package org.example.instagram.repository;
 
+import java.util.Optional;
 import org.example.instagram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/org/example/instagram/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package org.example.instagram.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.instagram.entity.User;
+import org.example.instagram.repository.UserRepository;
+import org.example.instagram.security.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new CustomUserDetails(user);
+
+    }
+
+}
